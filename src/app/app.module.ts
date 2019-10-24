c177894ae70d8f67c91af1fbf0016d6a9be42d0b
WuRuidongReyes <963211782@qq.com>
2019-10-24 15:03:06 +0800

    去掉appmodules中宝航的导致无法运行的animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -19,7 +18,6 @@ import { SearchBoxComponent } from './search-box/search-box.component';
    imports: [
       BrowserModule,
       AppRoutingModule,
-      Animation
    ],
    providers: [],
    bootstrap: [
